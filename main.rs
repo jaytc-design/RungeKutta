696ed5e3bb97404cfd80d6abc713b414ee38a6ed
John <johnthomascanton@gmail.com>
2024-03-05 00:30:53 -0500

    Working interface

--- main.rs
+++ main.rs
@@ -1,33 +1,49 @@
-use std::io;
+use ndarray::prelude::*;
+use ndarray::Array;
 
 fn main() {
-    // Take in user input
-    let (mut m1, mut m2, mut m3, mut m4, mut m): (f64, f64, f64, f64, f64);
+    // println!("Enter x0,y0,xn,h:");
+    // let (x0, y0, xn, h) = (inp(), inp(), inp(), inp());
 
-    println!("Enter x0,y0,xn,h:");
-    let (x0, y0, xn, h) = (inp(), inp(), inp(), inp());
-
-    let (mut x, mut y) = (x0, y0);
-
-    while x<xn
+    for i in 
+        rk4(|_t, y| y.clone(), 
+            arr1(&[0.0, 1.0]), 
+            0.0, 
+            10.0, 
+            100.0
+        )
     {
-        m1=f(x0,y0);
-        m2=f(x0+h/2.0,y0+m1*h/2.0);
-        m3=f(x0+h/2.0,y0+m2*h/2.0);
-        m4=f(x0+h,y0+m3*h);
-        m=(m1+2.0*m2+2.0*m3+m4)/6.0;
-        y+=m*h;
-        x+=h;
-        println!("{}\n{}\n",x,y);
+        println!("{:?}", i);
     }
 }
 
-fn f(x: f64, y: f64) -> f64 {
-    (x-y)/(x+y)
-}
+fn rk4<T, F>(f: F , initial: Array<f64, T>, a: f64, b: f64, n: f64) -> Vec<Array<f64, T>>
+where 
+    F: Fn(f64, &Array<f64, T>) -> Array<f64, T>, // mutating closure
+    T: Dimension // n-dimensions
+{
+    let h = (b-a)/n; // Step size
 
-fn inp() -> f64 {
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-    input.trim().parse().unwrap()
+    let mut v: Vec<Array<f64, T>> = Vec::new(); // Record of n-dimension arrays
+    v.push(initial.clone());
+
+    let mut t: f64 = a; // Time
+    let mut y: Array<f64, T> = initial.clone(); // Initial value to be changed
+
+    while t<b-h
+    {
+        let m1=f(t, &y);
+        let m2=f(t+h/2.0, &(&y+&m1*h/2.0));
+        let m3=f(t+h/2.0,&(&y+&m2*h/2.0));
+        let m4=f(t+h,&(&y+&m3*h));
+        let m=(m1+2.0*m2+2.0*&m3+m4)/6.0;
+
+        y = y+h*m;
+
+        v.push(y.clone());
+
+        t+=h;
+    }
+
+    v
 }
